a308d5785ee48ab2e76041d2d7a3a5da5e34f67e
marcodinicola88 <marco.dinicola@yahoo.it>
2023-07-18 12:42:57 +0200

    implementazione modello entities

---
+++ BuildWeek-Java1/src/main/java/entities/Zona.java
@@ -0,0 +1,45 @@
+package entities;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Table(name = "zone")
+public class Zona {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "zona_id")
+	private Long id;
+
+	@Column(length = 100)
+	private String nome;
+
+	@OneToMany(mappedBy = "capolinea")
+	private Set<Tratta> capolinea;
+
+	@OneToMany(mappedBy = "partenza")
+	private Set<Tratta> partenze;
+
+	public Zona(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+}
