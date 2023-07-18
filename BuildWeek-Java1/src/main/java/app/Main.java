7f3a1e66ebb06b3b8378e070e41adb6dc7b03513
marcodinicola88 <marco.dinicola@yahoo.it>
2023-07-18 10:56:23 +0200

    commit iniziale

--- BuildWeek-Java1/src/main/java/app/Main.java
+++ BuildWeek-Java1/src/main/java/app/Main.java
@@ -1,9 +1,26 @@
 package app;
 
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import utils.JpaUtil;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
+		EntityManager em = emf.createEntityManager();
+		Scanner sc = new Scanner(System.in);
+
+		try {
+
+		} finally {
+			em.close();
+			emf.close();
+			sc.close();
+		}
 
 	}
 
